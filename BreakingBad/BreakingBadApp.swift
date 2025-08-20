37f357c44a0b727fc976a6e62901e8f5f31df331
Stas <ragioniere1980@gmail.com>
2025-08-20 17:25:41 +0300

    first commit

---
+++ BreakingBad/BreakingBadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadApp.swift
+//  BreakingBad
+//
+//  Created by Станислав Леонов on 20.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
