20ca45b11ccf7f82a6151cd7d864208822daf62b
Stas <ragioniere1980@gmail.com>
2025-08-21 10:30:48 +0300

    add tab view and icons
    add tab view and icons

--- BreakingBad/ContentView.swift
+++ BreakingBad/ContentView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
+        TabView {
+            Tab("Breaking Bad", systemImage: "tortoise") {
+                Text("Breaking Bad View")
+                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
+            }
+            Tab("Better Call Saul", systemImage: "briefcase") {
+                Text("Better Call Saul View")
+                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
+            }
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
