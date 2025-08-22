eebfcb1d71fbf22739ebc494a7f8fef0d628c600
Stas <ragioniere1980@gmail.com>
2025-08-22 16:26:44 +0300

    add custom decoder
    add custom decoder

--- BreakingBad/Char.swift
+++ BreakingBad/Char.swift
@@ -16,4 +16,32 @@ struct Char: Decodable {
     let status: String
     let portrayedBy: String
     var death: Death?
+    
+    enum CodingKeys: CodingKey {
+        case name
+        case bithday
+        case occupation
+        case images
+        case aliases
+        case status
+        case portrayedBy
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.bithday = try container.decode(String.self, forKey: .bithday)
+        self.occupation = try container.decode([String].self, forKey: .occupation)
+        self.images = try container.decode([URL].self, forKey: .images)
+        self.aliases = try container.decode([String].self, forKey: .aliases)
+        self.status = try container.decode(String.self, forKey: .status)
+        self.portrayedBy = try container.decode(String.self, forKey: .portrayedBy)
+        
+        
+        let deathDecoder = JSONDecoder()
+        deathDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let deathData = try Data(contentsOf: Bundle.main.url(forResource: "sampledeath", withExtension: "json")!)
+        death = try deathDecoder.decode(Death.self, from: deathData)
+    }
 }
